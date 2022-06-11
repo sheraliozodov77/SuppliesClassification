bbebd37593dc81c6d79c0dc4a5b34d9937ef5a9e
Sherali Ozodov <107246911+sheraliozodov77@users.noreply.github.com>
2022-06-11 16:16:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ if plat == 'Linux': pathlib.PosixPath = pathlib.WindowsPath
 
     
 ## title
-st.title('Ishxona, musiqa va oshxona jihozlarini klassifikatsiya qiluvchi model')
+st.title('Jihozlarini klassifikatsiya qiluvchi model')
+
+st.text('Ushbu model ishxona, musiqa va oshxona jihozlarini klassifikatsiya qiladi')
 
 # rasmni joylash
 file = st.file_uploader('Rasm yuklash', type=['png','jpeg','gif','svg','jpg'])
 if file:
+    with st.spinner(text='In progress'):
+        time.sleep(1)
     st.image(file)
     # PIL konvert
     img = PILImage.create(file)
@@ -27,4 +31,5 @@ if file:
     #plotting
     fig = px.bar(x=probs*100, y=model.dls.vocab)
     st.plotly_chart(fig)
+
 
